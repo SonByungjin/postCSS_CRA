d45d7639df0a37aebb8b4d9013e2f8190941ca44
손병진 <sonbyeongjin@sonbyeongjin-ui-iMac.local>
2021-02-02 14:16:59 +0900

    rebase try

--- src/containers/support/Support.tsx
+++ src/containers/support/Support.tsx
@@ -1,13 +1,53 @@
 import React, { Component } from 'react';
+import {
+    Button,
+    ClickAwayListener,
+    createStyles,
+    MenuItem,
+    Paper,
+    Switch,
+    TextField,
+    Theme,
+    Typography,
+    WithStyles,
+    withStyles
+} from '@material-ui/core';
 
-export class Support extends Component {
+const styles = (theme:Theme) => createStyles({
+    root : {
+        flexGrow : 1
+    }
+});
+
+interface StyleProps extends WithStyles<typeof styles>{
+    theme : Theme;
+}
+
+export class Support extends Component<StyleProps> {
     render() {
         return (
-            <div>
-                <h1>
-                    고객센터 페이지
-                </h1>
-            </div>
+            <>
+             <TextField 
+            //  InputProps={{
+            //      readOnly : true
+            //  }} 
+            //  defaultValue="test" 
+            type='number'
+            label="test"
+            margin='normal'
+            variant='outlined'
+            placeholder='select'
+            // value='testsets'
+            //  InputLabelProps-={{
+            //      shrink : true
+            //  }}
+             />
+             <Typography
+                variant='h1'
+             >
+                 lets go
+             </Typography>
+            </>
         );
     }
 }
