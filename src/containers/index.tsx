b27ea44ecfc60a146092c399e502931c2eb3d308
손병진 <sonbyeongjin@sonbyeongjin-ui-iMac.local>
2021-02-22 12:57:53 +0900

    comment practice

--- src/containers/index.tsx
+++ src/containers/index.tsx
@@ -1,18 +1,20 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 // import {Route, Switch} from 'react-router';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-import {SigInScreen} from './signin/Signin';
-import {Support} from './support/Support';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { SigInScreen } from "./signin/Signin";
+import { Support } from "./support/Support";
 
 export class Layout extends Component {
-    render() {
-        return (
-            <Router>
-                <Switch>
-                    <Route exact path='/' component={SigInScreen}/>
-                    <Route exact path='/support' component={Support}/>
-                </Switch>
-            </Router>
-        );
-    }
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/" component={SigInScreen} />
+          <Route exact path="/support" component={Support} />
+        </Switch>
+      </Router>
+    );
+  }
 }
+
+// comment practice
