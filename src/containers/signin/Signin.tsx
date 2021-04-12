6dc0215e54e32bdc6a628ca2fe10b759d3b5f6ca
손병진 <sonbyeongjin@sonbyeongjin-ui-iMac.local>
2021-04-12 19:12:33 +0900

    git stash practice

--- src/containers/signin/Signin.tsx
+++ src/containers/signin/Signin.tsx
@@ -1,72 +1,72 @@
-import React, { Component } from 'react';
-import {connect, MapStateToProps, MapDispatchToPropsFunction} from 'react-redux';
-import {Login} from '../../components/login/Login';
-import {LoginResult} from '../../components/loginResult/LoginResult';
-import {NavBar} from '../../components/navBar/NavBar';
-import {Footer} from '../../components/footer/Footer';
-import {loginFetch, loginReload} from '../../modules/action';
-import {RootState} from '../../modules/reducer';
-import {selectLoading, selectStatus, selectMsg} from '../../modules/selector';
+import React, { Component } from "react";
+import {
+  connect,
+  MapStateToProps,
+  MapDispatchToPropsFunction,
+} from "react-redux";
+import { Login } from "../../components/login/Login";
+import { LoginResult } from "../../components/loginResult/LoginResult";
+import { NavBar } from "../../components/navBar/NavBar";
+import { Footer } from "../../components/footer/Footer";
+import { loginFetch, loginReload } from "../../modules/action";
+import { RootState } from "../../modules/reducer";
+import { selectLoading, selectStatus, selectMsg } from "../../modules/selector";
 
-interface ReduxProps{
-    loading : boolean;
-    status : boolean;
-    msg : string;
-};
+interface ReduxProps {
+  loading: boolean;
+  status: boolean;
+  msg: string;
+}
 
 interface DispatchProps {
-    loginFetch : typeof loginFetch;
-    loginReload : typeof loginReload;
-};
+  loginFetch: typeof loginFetch;
+  loginReload: typeof loginReload;
+}
 
 type Props = ReduxProps & DispatchProps;
 
 class Signin extends Component<Props> {
-    render() {
-        const {
-            loginReload,
-            loginFetch,
-            loading,
-            status,
-            msg
-        } = this.props;
-        return (
-            <>
-                <NavBar />
-                <div className='pg-signin'>
-                    <div className='pg-signin_box'>
-                        {loading?
-                        <Login 
-                        loginFetch={loginFetch} />
-                        : <LoginResult 
-                        loginReload={loginReload}
-                        loginStatus = {status}
-                        msg = {msg}
-                        />
-                        }
-                    </div>
-                </div>
-                <Footer />
-            </>
-        );
-    };
-};
+  render() {
+    const { loginReload, loginFetch, loading, status, msg } = this.props;
+    return (
+      <>
+        <NavBar />
+        <div className="pg-signin">
+          <div className="pg-signin_box">
+            {loading ? (
+              <Login loginFetch={loginFetch} />
+            ) : (
+              <LoginResult
+                loginReload={loginReload}
+                loginStatus={status}
+                msg={msg}
+              />
+            )}
+            <h1>git</h1>
+          </div>
+        </div>
+        <Footer />
+      </>
+    );
+  }
+}
 
-const mapStateToProps : MapStateToProps<ReduxProps,{},RootState>
-    = (state: RootState) : ReduxProps => ({
-        loading : selectLoading(state),
-        status : selectStatus(state),
-        msg : selectMsg(state),
-    });
+const mapStateToProps: MapStateToProps<ReduxProps, {}, RootState> = (
+  state: RootState
+): ReduxProps => ({
+  loading: selectLoading(state),
+  status: selectStatus(state),
+  msg: selectMsg(state),
+});
 
-const mapDispatchToProps : MapDispatchToPropsFunction<DispatchProps, {}>
-    = dispatch => ({
-        loginFetch : ({email, password}) =>
-        dispatch(loginFetch({email, password})),
-        loginReload: () =>
-        dispatch(loginReload()),
-    });
+const mapDispatchToProps: MapDispatchToPropsFunction<DispatchProps, {}> = (
+  dispatch
+) => ({
+  loginFetch: ({ email, password }) =>
+    dispatch(loginFetch({ email, password })),
+  loginReload: () => dispatch(loginReload()),
+});
 
 const SigInScreen = connect(mapStateToProps, mapDispatchToProps)(Signin);
 
-export {SigInScreen};
+export { SigInScreen };
